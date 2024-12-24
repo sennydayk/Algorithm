be1bbfb50998729621ebf0b78c5e772ad1dc1c57
sennydayk <141703065+sennydayk@users.noreply.github.com>
2024-12-24 15:18:11 +0900

    [level 0] Title: 몫 구하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -1,5 +1,8 @@
 function solution(num1, num2) {
-    var answer = 0;
-    answer = ~~(num1 / num2)
-    return answer;
+    return ~~(num1 / num2)
 }
+
+// ~ 연산자
+// ~n -> -(n+1) 반환
+// ex. n = 5  ~n = -6  ~~n = 5
+// 따라서 ~~을 붙이면 소수점 아래를 버림한 정수값을 반환할 수 있음
