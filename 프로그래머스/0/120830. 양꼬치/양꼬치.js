8e6b096bb63be881a2d9ade855244aa823f8fe0c
sennydayk <141703065+sennydayk@users.noreply.github.com>
2024-12-25 21:47:54 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,11 +1,6 @@
 function solution(n, k) {
-    var answer = 0;
     if (n >= 10) {
-        var dc = 0;
-        dc = ~~(n/10) * 2000
-        answer = 12000 * n + 2000 * k - dc
-    } else {
-        answer = 12000 * n + 2000 * k
+       k -= ~~(n/10)
     }
-    return answer;
+    return 12000 * n + 2000 * k
 }
