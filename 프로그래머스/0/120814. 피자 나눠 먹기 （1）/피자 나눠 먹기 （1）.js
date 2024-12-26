f6d3796515af67742146cbf9af39900262001887
sennydayk <141703065+sennydayk@users.noreply.github.com>
2024-12-26 17:23:09 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,7 +1,9 @@
 function solution(n) {
-    if (n / 7 === ~~(n / 7)) {
-        return n / 7
-    } else {
-        return ~~(n / 7) + 1;
-    }
+    // if (n / 7 === ~~(n / 7)) {
+    //     return n / 7
+    // } else {
+    //     return ~~(n / 7) + 1;
+    // }
+    
+    return Math.ceil(n / 7)
 }
